37311b39285dfe87b7e4faa5201ae815f542637b
kriukov.igor <krukovigor321@gmail.com>
2024-03-27 14:48:08 +0300

    Tasks 1-3

--- SomePythonStuff/27.03.24_L5.py
+++ SomePythonStuff/27.03.24_L5.py
@@ -1,2 +1,46 @@
-print("Hello world")
-print("I did a thing")
+import random
+
+def task1():
+    vals = []
+    n = int(input("Enter your digit (0,9): "))
+    for i in range(5):
+        vals.append(random.randint(0,9))
+        print(vals[i], end = " ")
+    print (f"\n{n}")
+    if n in vals:
+        print("Correct!")
+    else:
+        print("Wrong!")
+    
+def task2():
+    vals = []
+    reps = []
+    f = False
+    for i in range(100):
+        vals.append(random.randint(0,9))
+    for i in range(100):
+        if vals[i] not in reps:
+            for j in range(100):
+                if vals[i] == vals[j]:
+                    reps.append(vals[j])
+                    break
+    for i in range(len(reps)):
+        print(reps[i], end = " ")
+        
+def task3():
+    days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    n = int(input("Weekend duration: "))
+    print("Your weekend:", end = " ")
+    for i in range(n):
+        print(days[-(i+1)], end = " ")
+    print("\nWorking days:", end = " ")
+    for i in range(len(days)-n):
+        print(days[i], end = " ")
+        
+def task4():
+    group1 = []
+    group2 = []
+    
+    
+
+task3()
